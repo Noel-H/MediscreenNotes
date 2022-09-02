623bc6da194b5b31e250020a74c28ad4ec61e915
Noel-H <noel.huberto@gmail.com>
2022-09-02 17:20:02 +0200

    Initial commit autofix

--- src/main/java/com/noelh/mediscreennotes/MediscreenNotesApplication.java
+++ src/main/java/com/noelh/mediscreennotes/MediscreenNotesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MediscreenNotesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MediscreenNotesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MediscreenNotesApplication.class, args);
+    }
 
 }
